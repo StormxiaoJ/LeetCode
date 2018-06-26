e0e7a2d6ecfc34378bb568332810afa34c04064b
StormxiaoJ <StormxiaoJ@gmail.com>
2018-06-26 10:58:18 +1000

    Add files via upload

---
+++ Java/35_Search Insert Position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        if(target > nums[nums.length-1]){
+            return nums.length;
+        }
+        while(start + 1 < end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] < target){
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if(nums[start] >= target){
+            return start;
+        } else {
+            return end;
+        }
+    }
+}
