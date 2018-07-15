4271597e9fd7cf418d010856343d52d3602c58ec
StormxiaoJ <StormxiaoJ@gmail.com>
2018-07-15 16:59:20 +1000

    Add files via upload

---
+++ Java/93_Restore IP Addresses.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<String>();
+        helper(s, 0, "", res);
+        return res;
+    }
+    public void helper(String s, int n, String out, List<String> res) {
+        if (n == 4) {
+            if (s.isEmpty()) res.add(out);
+            return;
+        }
+        for (int k = 1; k < 4; ++k) {
+            if (s.length() < k) break;
+            int val = Integer.parseInt(s.substring(0, k));
+            if (val > 255 || k != String.valueOf(val).length()) continue;
+            helper(s.substring(k), n + 1, out + s.substring(0, k) + (n == 3 ? "" : "."), res);
+        }
+    }
+}
