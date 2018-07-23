c3e615f726989fb27841238a6244cb0aa99e8c79
StormxiaoJ <StormxiaoJ@gmail.com>
2018-07-23 12:10:47 +1000

    Add files via upload

--- data structure and algorithm/BubbleSort.java
+++ data structure and algorithm/BubbleSort.java
@@ -1,17 +1,17 @@
-public class Solution{
-	pulbic void sortIntegers(int[] A){
-		while(true){
-			boolean exchange = false;
-			for(int i = 0; i < A.length-1 ; i++){
-				if(A[i] > A[i+1]){
-					int tmp = A[i];
-					A[i+1] = tmp;
-					exhcange = true;
-				}
-			}
-			if(!exchange){
-				break;
-			}
+public class bubbleSort {
+public	bubbleSort(){
+	 int a[]={49,38,65,97,76,13,27,49,78,34,12,64,5,4,62,99,98,54,56,17,18,23,34,15,35,25,53,51};
+	int temp=0;
+	for(int i=0;i<a.length-1;i++){
+		for(int j=0;j<a.length-1-i;j++){
+		if(a[j]>a[j+1]){
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
 		}
 	}
+	for(int i=0;i<a.length;i++)
+		System.out.println(a[i]);	
+}
 }
